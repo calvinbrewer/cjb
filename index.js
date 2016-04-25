2e1397f97de33daaf7a0803ca614e7939ea6d6c1
calvinbrewer <brewercalvinj@gmail.com>
2016-04-25 15:48:58 -0600

    Add portfolio

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+app.get('/portfolio', function(req,res){
+  res.sendFile(__dirname + '/public/portfolio.html');
+});
+
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
