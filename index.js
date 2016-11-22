bd510a4f9cc142a6bdf5d06408dba59ed8c9f62e
calvinbrewer <brewercalvinj@gmail.com>
2016-11-21 20:36:09 -0700

    Add favicon

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
 console.log('Server started!');
