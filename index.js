ff8b9562b2a2bf725fe00f02973f1b0f46fb7ca4
CJ Brewer <brewercalvinj@gmail.com>
2017-06-26 13:54:20 -0600

    Bring on domain and slow it way down

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(function(req,res,next){setTimeout(next,Math.floor(Math.random() * (2000 - 1000 + 1) + 1000))});
 app.use(express.static(__dirname + '/public'));
+
 app.listen(port);
 console.log('Server started!');
