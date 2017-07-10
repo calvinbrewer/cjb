eb25454d0c482c2d851b24dfcfea923f3eaf8f3a
CJ Brewer <brewercalvinj@gmail.com>
2017-07-10 12:08:03 -0600

    Add webinar demo material

--- index.js
+++ index.js
@@ -2,8 +2,44 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 
 var app = express();
+var pagecount = 1;
 
 app.use(function(req,res,next){setTimeout(next,Math.floor(Math.random() * (2000 - 1000 + 1) + 1000))});
+
+app.get('/pagecount', function (req, res, next) {
+    res.json({ pagecount: pagecount });
+    pagecount += 1;
+});
+
+app.get('/pagecount-esi', function (req, res, next) {
+    res.send("<span id='p'>" + pagecount + "</span>");
+    pagecount += 1;
+});
+
+app.get('/1.html', function (req, res, next) {
+    res.send("<h1>Hello everyone!</h1>\n<p>Page count: " + pagecount + "</p>");
+    pagecount += 1;
+});
+
+app.get('/2.html', function (req, res, next) {
+    var html = "<h1>Hello everyone!</h1>\n<p>Page count: <span id='p'></span></p>\n" +
+               "<script src='/assets/js/jquery-3.2.1.min.js'></script>\n" +
+               "<script>\n" +
+               "$(document).ready(function() {\n" +
+               "  $.getJSON('/pagecount', function(data) {\n" +
+               "    console.log(data);\n" +
+               "    $('#p').html(data.pagecount);\n" +
+               "  });\n"+
+               "});\n" +
+               "</script>";
+    res.send(html);
+});
+
+app.get('/3.html', function (req, res, next) {
+    var html = "<h1>Hello everyone!</h1>\n<p>Page count: <esi:include src='/pagecount'/></p>\n";
+    res.send(html);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
